1cad95cd9a418b5968ef5c8ea1d242b3d3f2f99d
Eduardo Soares Azzolin <eduduazzolin@gmail.com>
2025-02-26 21:16:12 -0300

    refac/código comentado

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,12 +2,15 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Dropdown from 'react-bootstrap/Dropdown';
-import { Link, NavLink } from 'react-router-dom';
+import {Link, NavLink} from 'react-router-dom';
 
 function AppNavbar() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+
+    <Navbar expand="md" className="bg-body-tertiary">
       <Container>
+
+        {/*logo*/}
         <Navbar.Brand as={Link} to="/home">
           <img
             src="/logo.png"
@@ -16,14 +19,26 @@ function AppNavbar() {
             alt="React Bootstrap logo"
           />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+
+        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
         <Navbar.Collapse id="basic-navbar-nav">
+
+          {/*links*/}
           <Nav className="me-auto">
             <Nav.Link as={NavLink} to="/home">Home</Nav.Link>
             <Nav.Link as={NavLink} to="/novo-registro">Novo Registro</Nav.Link>
             <Nav.Link as={NavLink} to="/relatorios">Relatórios Públicos</Nav.Link>
             <Nav.Link as={NavLink} to="/sobre">Sobre</Nav.Link>
           </Nav>
+
+
+          {/*usuário*/}
+
+          {/*<Nav className="ml-auto">*/}
+          {/*  <Nav.Link as={NavLink} to="/home">Login</Nav.Link>*/}
+          {/*</Nav>*/}
+
           <Nav className="ml-auto">
             <Dropdown align="end">
               <Dropdown.Toggle variant="gray" id="dropdown-basic" className="d-flex align-items-center gap-3">
@@ -43,7 +58,9 @@ function AppNavbar() {
               </Dropdown.Menu>
             </Dropdown>
           </Nav>
+
         </Navbar.Collapse>
+
       </Container>
     </Navbar>
   );
